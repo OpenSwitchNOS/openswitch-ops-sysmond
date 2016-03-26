4972cdb33ea14f5712dbc31c9813a1d4d9f19322
srinsriv <srinivasan.srivatsan@hpe.com>
2016-03-25 23:54:56 -0700

    new: dev: @wip System monitoring background daemon changes
    Change-Id: I497008b4723605a8df8a6bbfc98d9f7fb015a9b5
    Signed-off-by: srinsriv <srinivasan.srivatsan@hpe.com>

---
+++ src/cli/sysmond_vty.c
@@ -0,0 +1,163 @@
+/* SYSMOND CLI commands
+ *
+ * Copyright (C) 1997, 98 Kunihiro Ishiguro
+ * Copyright (C) 2015-2016 Hewlett Packard Enterprise Development LP
+ *
+ * GNU Zebra is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2, or (at your option) any
+ * later version.
+ *
+ * GNU Zebra is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Zebra; see the file COPYING.  If not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * File: sysmond_vty.c
+ *
+ * Purpose: To add sysmond CLI configuration and display commands
+ */
+
+#include <sys/wait.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+#include "vtysh/command.h"
+#include "memory.h"
+#include "vtysh/vtysh.h"
+#include "vtysh/vtysh_user.h"
+#include "vswitch-idl.h"
+#include "ovsdb-idl.h"
+#include "sysmond_vty.h"
+#include "smap.h"
+#include "openvswitch/vlog.h"
+#include "openswitch-idl.h"
+#include "ovsdb-data.h"
+#include "vtysh/vtysh_ovsdb_config.h"
+#include "vtysh/vtysh_ovsdb_if.h"
+
+/*================================================================================================*/
+/* SHOW CLI Implementations */
+static void
+vtysh_show_system_memory_handler()
+{
+}
+
+static void
+vtysh_show_system_cpu_handler()
+{
+}
+
+/*================================================================================================*/
+/* CLI Definitions */
+
+DEFUN ( vtysh_show_system_cpu,
+        vtysh_show_system_cpu_cmd,
+        "show system cpu",
+        SHOW_STR
+        SYSTEM_DISPLAY_STR
+        CPU_DISPLAY_STR
+      )
+{
+    vtysh_show_system_cpu_handler();
+    return CMD_SUCCESS;
+}
+
+DEFUN ( vtysh_show_system_memory,
+        vtysh_show_system_memory_cmd,
+        "show system memory",
+        SHOW_STR
+        SYSTEM_DISPLAY_STR
+        MEMORY_DISPLAY_STR
+      )
+{
+    vtysh_show_system_memory_handler();
+    return CMD_SUCCESS;
+}
+
+DEFUN ( vtysh_set_monitor_poll_timer,
+        vtysh_set_monitor_poll_timer_cmd,
+        "monitor poll-timer <5-15>",
+        MONITOR_DISPLAY_STR
+        POLL_TIMER_DISPLAY_STR
+        POLL_TIMER_RANGE_DISPLAY_STR
+      )
+{
+    /*
+     * int ret_code = CMD_SUCCESS;
+     */
+    /*
+     * ntp_cli_ntp_auth_enable_params_t ntp_auth_enable_params;
+     * ntp_auth_enable_get_default_parameters(&ntp_auth_enable_params);
+     *
+     * if (vty_flags & CMD_FLAG_NO_CMD) {
+     *     ntp_auth_enable_params.no_form = 1;
+     * }
+     *
+     * ret_code = vtysh_ovsdb_ntp_auth_enable_set(&ntp_auth_enable_params);
+     */
+    /*
+     * return ret_code;
+     */
+    return CMD_SUCCESS;
+}
+
+
+DEFUN_NO_FORM ( vtysh_set_monitor_poll_timer,
+        vtysh_set_monitor_poll_timer_cmd,
+        "monitor poll-timer <5-15>",
+        MONITOR_DISPLAY_STR
+        POLL_TIMER_DISPLAY_STR
+        POLL_TIMER_RANGE_DISPLAY_STR
+      );
+
+/*================================================================================================*/
+
+/*
+ * Function       : sysmond_vty_init
+ * Responsibility : install all the CLIs in the respective contexts.
+ */
+
+void
+sysmond_vty_init (void)
+{
+    install_element (VIEW_NODE, &vtysh_show_system_cpu_cmd);
+    install_element (ENABLE_NODE, &vtysh_show_system_cpu_cmd);
+
+    install_element (VIEW_NODE, &vtysh_show_system_memory_cmd);
+    install_element (ENABLE_NODE, &vtysh_show_system_memory_cmd);
+
+    install_element (CONFIG_NODE, &vtysh_set_monitor_poll_timer_cmd);
+    install_element (CONFIG_NODE, &no_vtysh_set_monitor_poll_timer_cmd);
+}
+
+/*================================================================================================*/
+
+/* Initialize ops-sysmond cli node.
+ */
+void cli_pre_init(void)
+{
+    /* ops-sysmond doesn't have any context level cli commands.
+     * To load ops-sysmond cli shared libraries at runtime, this function is required.
+     */
+}
+
+/* Initialize ops-sysmond cli element.
+ */
+void cli_post_init(void)
+{
+    install_element (VIEW_NODE, &vtysh_show_system_cpu_cmd);
+    install_element (ENABLE_NODE, &vtysh_show_system_cpu_cmd);
+
+    install_element (VIEW_NODE, &vtysh_show_system_memory_cmd);
+    install_element (ENABLE_NODE, &vtysh_show_system_memory_cmd);
+
+    install_element (CONFIG_NODE, &vtysh_set_monitor_poll_timer_cmd);
+    install_element (CONFIG_NODE, &no_vtysh_set_monitor_poll_timer_cmd);
+}
+
+/*================================================================================================*/
