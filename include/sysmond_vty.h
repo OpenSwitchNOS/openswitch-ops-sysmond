54e4f1972a167a902d37a7552e0a43f576a42f58
srinsriv <srinivasan.srivatsan@hpe.com>
2016-03-31 16:45:56 -0700

    new: dev: Support for show system cpu/memory
    Change-Id: I231a15ca39e0b279aa2d4ab47875e1135a9f87bf
    Signed-off-by: srinsriv <srinivasan.srivatsan@hpe.com>

---
+++ include/sysmond_vty.h
@@ -0,0 +1,29 @@
+/* SYSMOND CLI commands header file
+ *
+ * Copyright (C) 2015-2016 Hewlett Packard Enterprise Development LP
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * File: sysmond_vty.h
+ *
+ * Purpose:  To add declarations required for sysmond_vty.c
+ */
+
+#ifndef _SYSMOND_VTY_H
+#define _SYSMOND_VTY_H
+#define SYSTEM_DISPLAY_STR "System information\n"
+#define CPU_DISPLAY_STR "System CPU usage information\n"
+#define MEMORY_DISPLAY_STR "System Memory usage information\n"
+#endif // _SYSMOND_VTY_H
